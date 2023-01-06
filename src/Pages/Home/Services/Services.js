195cbb04201e8fc59a635492991687ad7fb1c20f
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-01-06 13:46:19 +0600

    new comment

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Services.css';
+
+const Services = () => {
+    
+    return (
+        <div>
+            <h2>Services
+            </h2>
+        </div>
+    );
+};
+
+export default Services;
